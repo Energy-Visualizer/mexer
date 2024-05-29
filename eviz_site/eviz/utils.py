8077551213a256fbfec72ed5340cdf98f96395ef
Edom Maru <eam43@calvin.edu>
2024-05-29 11:25:06 -0400

    untils

---
+++ eviz_site/eviz/utils.py
@@ -0,0 +1,12 @@
+from time import time
+
+def time_view(v):
+
+    def wrap(*args, **kwargs):
+        t0 = time()
+        ret = v(*args, **kwargs)
+        t1 = time()
+        print(f"Time to run {v.__name__}: {t1 - t0}")
+        return ret
+
+    return wrap
