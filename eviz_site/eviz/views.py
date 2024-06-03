5ef11560f9c1986644869520faf2bafaa5be96f5
Kenny Howes <kmh67@calvin.edu>
2024-06-03 10:23:53 -0400

    quick fix

--- eviz_site/eviz/views.py
+++ eviz_site/eviz/views.py
@@ -92,47 +92,39 @@ def get_psut_data(request):
 
     return render(request, "./test.html", context)
 
+@time_view
+def _la_extraction(request):
 
-# def test_matrix_sum(m):
-    
-#     assert(m.size == 23)
-#     assert(int(IndexTranslator.translate(m, "Charcoal", "Charcoal production plants")) == 16108)
+    mat_context = {
+        "dataset":2, # CLPFU
+        "country":49, # GHA
+        "method":1, # PCM
+        "energy_type":2, # X
+        "last_stage":2, # Final
+        "ieamw":1, # IEA
+        "includes_neu":1, # True
+        "year":2015
+    }
 
-#     return "Passed all tests"
+    test_mat_context = {
+        "dataset":2, # CLPFU
+        "country":49, # GHA
+        "method":1, # PCM
+        "energy_type":1, # E
+        "last_stage":2, # Final
+        "ieamw":3, # Both
+        "includes_neu":1, # True
+        "year":1995
+    }
 
-# @time_view
-# def _la_extraction(request):
-
-#     mat_context = {
-#         "dataset":2, # CLPFU
-#         "country":49, # GHA
-#         "method":1, # PCM
-#         "energy_type":2, # X
-#         "last_stage":2, # Final
-#         "ieamw":1, # IEA
-#         "includes_neu":1, # True
-#         "year":2015
-#     }
-
-#     test_mat_context = {
-#         "dataset":2, # CLPFU
-#         "country":49, # GHA
-#         "method":1, # PCM
-#         "energy_type":1, # E
-#         "last_stage":2, # Final
-#         "ieamw":3, # Both
-#         "includes_neu":1, # True
-#         "year":1995
-#     }
-
-#     u = get_matrix(
-#         **test_mat_context,
-#         matrix_name=RUVY.U
-#     )
-#     v = get_matrix(
-#         **test_mat_context,
-#         matrix_name=RUVY.V
-#     )
+    u = get_matrix(
+        **test_mat_context,
+        matrix_name=RUVY.U
+    )
+    v = get_matrix(
+        **test_mat_context,
+        matrix_name=RUVY.V
+    )
 
     sum = u.T + v
 
@@ -140,4 +132,4 @@ def get_psut_data(request):
 
     context = { "passed": test_matrix_sum(sum), "sum": s }
 
-#     return render(request, "./la_extract.html", context)
+    return render(request, "./la_extract.html", context)
