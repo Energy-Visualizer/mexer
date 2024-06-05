f9bb1e34507b53e34cdd6e27583675ab2f0292f7
Kenny Howes <kmh67@calvin.edu>
2024-06-05 09:31:46 -0400

    start on viz page

--- eviz_site/eviz/urls.py
+++ eviz_site/eviz/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.get_psut_data),
+    path("", views.index),
     path("psut_data", views.get_psut_data),
-    path("viz", views.temp_viz)
+    path("viz", views.temp_viz),
 ]
