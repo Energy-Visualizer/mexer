b20cf17a773af53e7f9761af3c5d75a18ce204a2
Kenny Howes <kmh67@calvin.edu>
2024-06-20 10:27:22 -0400

    fixed urls

--- eviz_site/eviz/urls.py
+++ eviz_site/eviz/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     # main pages
     path('', views.index, name='home'),
-    path("visualizer", views.visualizer, name="visualizer"),
+    path("visualizer/", views.visualizer, name="visualizer"),
 
     # auth related pages
     path('login/', views.user_login, name='login'),
@@ -18,7 +18,7 @@ urlpatterns = [
     re_path(r"static/css/([^(\.css)]*\.css)", views.handle_css_static),
 
     # api services
-    path("plot/", views.get_plot),
-    path("data/", views.get_data),
-    path("verify/", views.verify_email)
+    path("plot", views.get_plot),
+    path("data", views.get_data),
+    path("verify", views.verify_email)
 ]
